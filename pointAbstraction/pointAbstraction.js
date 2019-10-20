0963647261818c9251282f70b1d4c831034f5531
Lev <agams92@gmail.com>
2019-10-20 16:22:26 +0300

    solved nahoooi

--- pointAbstraction/pointAbstraction.js
+++ pointAbstraction/pointAbstraction.js
@@ -1,14 +1,43 @@
-const makePoint = () => {};
+const makePoint = (x, y) => {
+  return {
+    x: x,
+    y: y
+  };
+};
 
-const getX = () => {};
+const getX = point => {
+  return point.x;
+};
 
-const getY = () => {};
+const getY = point => {
+  return point.y;
+};
 
-const toString = () => {};
+const toString = point => {
+  const coord = Object.values(point);
+  return `[${coord.join(", ")}]`;
+};
 
-const getDistance = () => {};
+const getDistance = (point1, point2) => {
+  const [x1, y1] = Object.values(point1);
+  const [x2, y2] = Object.values(point2);
+  const xDist = x1 - x2;
+  const yDist = y1 - y2;
+  return Math.sqrt(Math.pow(xDist, 2) + Math.pow(yDist, 2));
+};
 
-const getQuadrant = () => {};
+const getQuadrant = point => {
+  const [x, y] = Object.values(point);
+  if (x === 0 || y === 0) return null;
+  if (x < 0) {
+    if (y > 0) return 2;
+    if (y < 0) return 3;
+  }
+  if (x > 0) {
+    if (y > 0) return 1;
+    if (y < 0) return 4;
+  }
+};
 
 module.exports = {
   makePoint,
--- pseudoRouter/pseudoRouter.js
+++ pseudoRouter/pseudoRouter.js
@@ -1,3 +1,18 @@
-// const Router = function() {};
+const Router = function() {
+  return {
+    bind(url, method, cb) {
+      this.url = url;
+      this.method = method;
+      this.cb = cb;
+    },
+    runRequest(url, method) {
+      if (url === this.url && method === this.method) {
+        return this.cb();
+      } else {
+        return "Error 404: Not Found";
+      }
+    }
+  };
+};
 
 module.exports = Router;
